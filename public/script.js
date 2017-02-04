06bea887f7738786eae2c48b7f83cdff9d7ac6a5
Luis Ribés <ribes4694@gmail.com>
2017-02-04 22:49:13 +0100

    creat fitxer script.js on es guarden els scripts, i canviat index.html a carpeta public

---
+++ public/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+	    document.getElementById("text2").innerHTML = document.getElementById("inputButton").value;
+};
+
+function enterKey(event){
+   		if (event.keyCode == 13) {
+	    		document.getElementById("text2").innerHTML = document.getElementById("inputButton").value;
+			return false;	
+		}
+	}
