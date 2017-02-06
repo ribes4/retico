8eaf1ae7f6c4dd2f3a08bb935515ddef5f584d1a
Luis Ribés <ribes4694@gmail.com>
2017-02-06 23:41:30 +0100

    afegit chat

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('http');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', process.env.PORT || 3000);
 
@@ -8,8 +9,18 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
-})
+});
 
-http.createServer(app).listen(app.get('port'), function(){
+app.get('/chat', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "chat.html" );
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
