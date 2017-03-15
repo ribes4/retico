e63eeb6ce813559be655a3e1a41fa5bfc01a4174
Luis Ribés <ribes4694@gmail.com>
2017-03-15 10:13:57 +0100

    merge nova estructura

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,123 @@
-var socket = io();
+var playerName;
+var playerType = 'player';
+var playerNameInput = document.getElementById('playerNameInput');
+var socket;
 
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
 
+var c = document.getElementById('cvs');
+var canvas = c.getContext('2d');
+c.width = screenWidth; c.height = screenHeight;
 
-function enterKey(event){
+var KEY_ENTER = 13;
+
+var game = new Game();
+
+var player = {
+    id: -1,
+    x: screenWidth / 2,
+    y: screenHeight / 2,
+    screenWidth: screenWidth,
+    screenHeight: screenHeight,
+    target: {x: screenWidth / 2, y: screenHeight / 2}
+};
+var target = {x: player.x, y: player.y};
+
+//s'entrarà un cop s'hagi posat el nickname correcte i s'hagi donat al play
+function startGame() {
+	playerName = playerNameInput.value.replace(/(<([^>]+)>)/ig, '');
+	document.getElementById('gameAreaWrapper').style.display = 'block';
+	document.getElementById('startMenuWrapper').style.display = 'none';
+	socket = io();
+	SetupSocket(socket);
+	animloop();
+	//socket.emit('nickname',playerName)
+	socket.emit('respawn');
+}
+// check if nick is valid alphanumeric characters (and underscores)
+function validNick() {
+    var regex = /^\w*$/;
+    console.log('Regex Test', regex.exec(playerNameInput.value));
+    return regex.exec(playerNameInput.value) !== null;
+}
+
+window.onload = function() {
+    'use strict';
+
+    var btn = document.getElementById('startButton'),
+        nickErrorText = document.querySelector('#startMenu .input-error');
+
+    btn.onclick = function () {
+
+        // check if the nick is valid
+        if (validNick()) {
+            startGame();
+        } else {
+            nickErrorText.style.display = 'inline';
+        }
+    };
+
+    playerNameInput.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+
+        if (key === KEY_ENTER) {
+            if (validNick()) {
+                startGame();
+            } else {
+                nickErrorText.style.display = 'inline';
+            }
+        }
+    });
+};
+
+function SetupSocket(socket) {
+  game.handleNetwork(socket);
+}
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+function animloop(){
+    requestAnimFrame(animloop);
+    gameLoop();
+}
+
+function gameLoop() {
+  game.handleLogic();
+  game.handleGraphics(canvas);
+}
+
+window.addEventListener('resize', function() {
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    c.width = screenWidth;
+    c.height = screenHeight;
+}, true);
+
+window.addEventListener('resize', resize);
+
+function resize() {
+	if (!socket) return;
+
+	player.screenWidth = c.width = screenWidth = playerType == 'player' ? window.innerWidth : gameWidth;
+	player.screenHeight = c.height = screenHeight = playerType == 'player' ? window.innerHeight : gameHeight;
+
+	/*if (global.playerType == 'spectate') {
+		player.x = global.gameWidth / 2;
+		player.y = global.gameHeight / 2;
+	}*/
+
+	socket.emit('windowResized', { screenWidth: screenWidth, screenHeight: screenHeight });
+}
+//-------------------------------------------------------------------------------------------
+/* function enterKey(event){
    		if (event.keyCode == 13) {
 	    		play();
 			return false;	
@@ -10,54 +125,11 @@ function enterKey(event){
 	};
 
 function play() {
-	socket.emit('nickname',document.getElementById("nickname").value)
+	socket.emit('nickname',document.getElementById("nickname").value);
 	document.getElementById("nickname").value="";
 	location.href="/chat";
 };
 
-socket.on('id', function(id){
-	sessionStorage.setItem('idJugador', id)
-	alert("La teva id: "+id);
-});
-
-socket.on('pose',function(data){
-	for (i=0;i< data.length;i++){
-		var element = document.getElementById(data[i].id);
-		console.log(element)
-		if(!element){
-			$("#plataforma").append(data[i].jugadorPrincipal);
-		}
-		else{
-			element = data[i].jugadorPrincipal;
-	
-		}
-	}
-});
-
-socket.on("movent",function(data){
-	
-	var move={
-		left: data.style.left,
-		top: data.style.top
-	}
-	//$("#"+data.jugadorPrincipal).css(move);
-	$("#"+ data.id).css(move);
-});
-
-	  /*$('form').submit(function(){
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
-		return false;
-	  });
-	  socket.on('chat message', function(msg){
-		//$('#messages').append($('<li>').text(msg));
-	  });
-	  
-	  //per capturar quina tecla es prem
-	  $( "#m" ).keydown(function() {
-		var event = window.event ? window.event : e;
-		console.log(event.keyCode)
-	  });*/
 	  
 		
 
@@ -136,4 +208,5 @@ function docReady()
 	socket.emit("init_pose",obj);
 	
 	window.addEventListener('keydown', moveSelection);
-}
+}*/
+
--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Import utilities.
+var util = require('./lib/util');
+
+var users=[];
 var teamA=[];
 var teamB=[];
-var poses=[];
-var id=1;
+var sockets = {};
+//var poses=[];
+//var id=1;
+
 
 app.set('port', process.env.PORT || 3000);
 
@@ -16,23 +22,37 @@ app.get('/', function (req, res) {
 	res.sendFile( __dirname + "/" + "index.html" );
 });
 
-app.get('/chat', function (req, res) {
-	res.sendFile( __dirname + "/public" + "/chat.html" );
-});
-
-app.get('/prepartida', function (req, res) {
-	res.sendFile( __dirname + "/public" + "/prepartida.html" );
-})
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		
+
+	var currentPlayer = {
+		id: socket.id,
+		x: 0,
+		y: 0,
+		lastHeartbeat: new Date().getTime(),
+		target: {
+		    x: 0,
+		    y: 0
+		}
+	};	
+
+
+	//es crida quan el client ha emplenat el nick i vol començar el joc
+	socket.on('respawn', function (){
+		//busca si hi ha el jugador a la llista d'usuaris
+		if (util.findIndex(users, currentPlayer.id) > -1){
+			//si troba el client l'elimina
+			users.splice(util.findIndex(users, currentPlayer.id), 1);
+		}
+		//emet welcome, per enviar les dades al client
+		socket.emit('welcome', currentPlayer);
+		console.log('[INFO] User ' + currentPlayer.name + ' respawned!');
 	});
+
 	socket.on('nickname', function(nickname){
 		
 		var player = [id,nickname];
-		socket.emit('id',id);
+		socket.emit('welcome',id);
 		id++;
 		
 		if(teamA.length > teamB.length){
@@ -68,6 +88,89 @@ io.on('connection', function(socket){
 		}
 		io.emit("movent",data);
 	});
+
+
+
+
+	socket.on('gotit', function(player){
+		console.log('[INFO] Player ' + player.name + ' connecting!');
+		if (util.findIndex(users, player.id) > -1) {
+			console.log('[INFO] Player ID is already connected, kicking.');
+			socket.disconnect();
+		}
+		else {
+			console.log('[INFO] Player ' + player.name + ' connected!');
+			//sockets[player.id] = socket;
+
+			player.x = 0;
+			player.y = 0;
+			player.target.x = 0;
+			player.target.y = 0;
+	
+			currentPlayer = player;
+		    	currentPlayer.lastHeartbeat = new Date().getTime();
+		    	users.push(currentPlayer);
+
+			//assigna el jugador al equip amb menys jugadors
+			if(teamA.length > teamB.length){
+				teamB.push(currentPlayer);
+				mostrar();
+			}
+			else if(teamB.length > teamA.length){
+				teamA.push(currentPlayer);
+				mostrar();
+			}
+			else{
+				var rnd = Math.floor((Math.random() * 2) + 1);
+				if(rnd == 1){
+					teamA.push(currentPlayer);
+					mostrar();
+				}
+				else{
+					teamB.push(currentPlayer);
+					mostrar();
+				}
+			}
+		    	
+		    	
+			socket.emit('gameSetup', {
+				gameWidth: 5000,
+				gameHeight: 5000
+			});
+
+			console.log('Total players: ' + (teamA.length + teamB.length));
+		}
+	});
+	
+	socket.on('windowResized', function (data) {
+		currentPlayer.screenWidth = data.screenWidth;
+		currentPlayer.screenHeight = data.screenHeight;
+	});
+	
+	socket.on('disconnect', function(){
+		if (util.findIndex(users, currentPlayer.id) > -1){
+			//elimina el jugador de la llista de users
+			users.splice(util.findIndex(users, currentPlayer.id), 1);
+		}
+		if (util.findIndex(teamA, currentPlayer.id) > -1){
+			teamA.splice(util.findIndex(teamA, currentPlayer.id), 1);
+			}
+		if (util.findIndex(teamB, currentPlayer.id) > -1){
+			teamB.splice(util.findIndex(teamB, currentPlayer.id), 1);
+		}
+		
+        	console.log('[INFO] User ' + currentPlayer.name + ' disconnected!');
+
+        //socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name });
+	});
+	
+	// Heartbeat function, update everytime.
+	socket.on('0', function(target) {
+		currentPlayer.lastHeartbeat = new Date().getTime();
+		if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
+			currentPlayer.target = target;
+		}
+	});
 });
 
 function mostrar(){
@@ -75,13 +178,13 @@ function mostrar(){
 	console.log("\n\n\n\n\nEQUIP A:");
 	for(var i=0;i<teamA.length;i++){
 		var jugador = teamA[i];
-		console.log(jugador[0]+": "+jugador[1]);
+		console.log(jugador.id+": "+jugador.name);
 	}
 	
 	console.log("\nEQUIP B:");
 	for(var j=0;j<teamB.length;j++){
 		var jugador2 = teamB[j];
-		console.log(jugador2[0]+": "+jugador2[1]);
+		console.log(jugador2.id+": "+jugador2.name);
 	}
 }
 
